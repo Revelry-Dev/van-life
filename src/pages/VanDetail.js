ecd7604aaae6a089cf6384496b76423a222b6b2d
Jamie <jmeleacc@gmail.com>
2023-03-12 14:19:52 -0500

    complete van detail page

---
+++ src/pages/VanDetail.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function VanDetail() {
+	
+	const [van, setVan] = useState(null)
+	
+	
+	
+	const params = useParams()
+	
+	
+	
+	useEffect(() => {
+		fetch(`/api/vans/${params.id}`)
+			.then(res => res.json())
+			.then(data => setVan(data.vans))
+		}, [params.id])
+	
+	console.log(van)
+	
+	return (
+			<div className="van-detail-container">
+				{van ? (
+					<div className="van-detail">
+						<img src={van.imageUrl} alt={van.name} />
+						<i className={`van-type ${van.type} selected`}>{van.type}</i>
+						<h2>{van.name}</h2>
+						<p className="van-price"><span>${van.price}</span>/day</p>
+						<p>{van.description}</p>
+						<button className="link-button">Rent this van</button>
+					</div>
+				) : <h2>Loading...</h2>}
+			</div>
+		)
+	}
--- src/pages/Vans.js
+++ src/pages/Vans.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 
 export default function Vans() {
     const [vans, setVans] = useState([])
@@ -9,14 +10,18 @@ export default function Vans() {
     }, [])
 
     const vanElements = vans.map(van => (
+        
         <div key={van.id} className="van-tile">
+        <Link to={`/vans/${van.id}`}>
             <img src={van.imageUrl} alt={van.name} />
             <div className="van-info">
                 <h3>{van.name}</h3>
                 <p>${van.price}<span>/day</span></p>
             </div>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
+       
     ))
 
     return (
