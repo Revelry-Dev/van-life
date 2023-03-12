4464d1ebec10d23d2ace189deaf4fcc58ed3358c
Jamie <jmeleacc@gmail.com>
2023-03-12 12:32:07 -0500

    Add Vans page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './index.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
 
+//import "./server"
 
 export default function App() {
   return (
@@ -12,11 +14,13 @@ export default function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
